7b62bdd59519f56a6285b65198ed18a8bff087d3
Windula Dissanayake <charithwinwindula@gmail.com>
2022-07-18 02:11:00 +0530

    commit

---
+++ world_time/lib/services/world_time.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime{
+
+  String location; //location name for the UI
+  String flag; //url to an asset flag icon
+  String url; //location url for api endpoints
+  String time = ""; //the time in that location
+
+  WorldTime({required this.location, required this.flag, required this.url });
+
+  Future<void> getTime() async{
+
+    //make the request
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(datetime);
+    //print(offset);
+
+    //create datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    //print(now);
+
+    //set the time property
+    time = now.toString();
+
+  }
+
+}
